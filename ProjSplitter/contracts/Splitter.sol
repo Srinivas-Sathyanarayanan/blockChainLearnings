a7e96f8137a04248497b98d2eaf49f59d0639585
Srinivas Sathyanarayanan <sathi.srinivas32@gmail.com>
2018-07-07 00:50:17 -0700

    adding files

---
+++ ProjSplitter/contracts/Splitter.sol
@@ -0,0 +1,52 @@
+//This is Splitter Contract to split the amount in to half sent from Alice to Bob & Carol
+pragma solidity ^0.4.19;
+contract Splitter{
+
+	function owned() public { owner = msg.sender; }
+  	  address owner;
+
+
+	mapping (address => uint) public addresses;
+
+	function split(address receiverOne, address receiverTwo)  public  payable returns(bool isSuccessfull){
+	      
+		require(msg.value>0);
+		
+		uint remAmt = msg.value % 2;
+		uint splitAmt = (msg.value - remAmt) / 2;
+
+		if (remAmt > 0){
+			addresses[owner] += remAmt;}
+
+		addresses[receiverOne] += splitAmt;
+		
+		addresses[receiverTwo] += splitAmt;
+
+		return true;
+	
+	}
+	
+//Funds withdraw
+	function withdrawFunds() public {
+
+	    require(addresses[msg.sender]>0);
+	    uint amount = addresses[msg.sender];
+
+	 
+	    addresses[msg.sender] = 0;
+	  
+	    msg.sender.transfer(amount);
+
+	  }
+
+
+
+
+
+	//Self destruct function to be called only by owner
+  	function kill() public{ 
+		if (msg.sender == owner) selfdestruct(owner);
+		 }
+	
+}
+
